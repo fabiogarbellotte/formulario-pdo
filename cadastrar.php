0086a43f0dfb1f4fe60e1f2bd4ef5025c3ff54b4
fabiogarbellotte <fabiogarbellotte@gmail.com>
2022-08-22 22:57:27 -0300

    formulario

--- cadastrar.php
+++ cadastrar.php
@@ -1,6 +1,6 @@
-<<?php
-    require_once 'Usuario.php';
-    $u = new Usuario;
+<?php
+require_once 'Usuario.php';
+$u = new Usuario;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,44 +15,60 @@
 
 <body>
     <div id="corpo-cadastro">
-        <form method="POST" >
+        <form method="POST">
             <h1>CADASTRAR</h1>
             <input type="text" name="Nome" id="" placeholder="Nome completo" maxlength="30">
             <input type="tel" name="Telefone" id="" placeholder="Telefone" maxlength="30">
             <input type="email" name="Email" id="" placeholder="Email" maxlength="40">
             <input type="password" name="Senha" id="" placeholder="Senha" maxlength="15">
             <input type="password" name="ConfSenha" id="" placeholder="Confirmar Senha" maxlength="15">
-            <input type="submit"  value="CADASTRAR">
+            <input type="submit" value="CADASTRAR">
             <a href="index.php">Voltar e Acessar </a>
         </form>
     </div>
     <?php
-if(isset($_POST['Nome'])){
-    $Nome = addslashes($_POST['Nome']);
-    $Telefone  = addslashes($_POST['Telefone']);
-    $Email  = addslashes($_POST['Email']);
-    $Senha  = addslashes($_POST['Senha']);
-    $ConfirmarSenha  = addslashes($_POST['ConfSenha']);
+    if (isset($_POST['Nome'])) {
+        $Nome = addslashes($_POST['Nome']);
+        $Telefone  = addslashes($_POST['Telefone']);
+        $Email  = addslashes($_POST['Email']);
+        $Senha  = addslashes($_POST['Senha']);
+        $ConfirmarSenha  = addslashes($_POST['ConfSenha']);
 
-    if(!empty($Nome)&& !empty($Telefone)&& !empty($Email) && !empty($Senha) && !empty($ConfirmarSenha)){
-        $u->conectar("banco","localhost","root","");
-        if ($u->$msg == ""){
-            if($Senha == $ConfirmarSenha){
-                    if($u->cadastrar($Nome,$Telefone,$Email,$Senha)){
-                      echo "Cadastrado com sucesso! Acesse para entrar!";
-                    }else{
-                        echo "Email ja cadastrado";
+        if (!empty($Nome) && !empty($Telefone) && !empty($Email) && !empty($Senha) && !empty($ConfirmarSenha)) {
+            $u->conectar("banco", "localhost", "root", "");
+            if ($u->msg == "") {
+                if ($Senha == $ConfirmarSenha) {
+                    if ($u->cadastrar($Nome, $Telefone, $Email, $Senha)) {
+    ?>
+                        <div id="msg-sucesso">
+                            Cadastrado com sucesso! Acesse para entrar!
+                        </div>
+                    <?php
+                    } else {
+                    ?>
+                        <div class="msg-erro">
+                            Email já cadastrado!
+                        </div>
+                    <?php
                     }
-            }else{
-                echo "Senha e confirmar senha não conferem!";
+                } else {
+                    ?>
+                    <div class="msg-erro">
+                        Senha e confirmar senha não conferem!
+                    </div>
+            <?php
+                }
+            } else {
+                echo "Erro " . $u->msg;
             }
-        }else{
-            echo "Erro ".$u->$msg;
+        } else {
+            ?>
+            <div class="msg-erro">
+                Preencha todos os campos!
+            </div>
+    <?php
         }
-}else{
-    echo "Preencha todos os dados";
-}
-}
+    }
     ?>
 </body>
 
