d0ed1a898fc8d54c1d4f9a85cd5c2c1f9d968dea
fabiogarbellotte <fabiogarbellotte@gmail.com>
2022-08-22 23:36:25 -0300

    formulario

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'Usuario.php';
+$u = new Usuario;
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -16,10 +20,41 @@
     <div>
         <form method="POST" >
             <h1>ENTRAR</h1>
-            <input type="text" name="" id="" placeholder="Usuario">
-            <input type="password" name="" id="" placeholder="Senha">
+            <input type="text" name="Email" id="" placeholder="Email">
+            <input type="password" name="Senha" id="" placeholder="Senha">
             <input type="submit" value="ACESSAR">
             <a href="cadastrar.php">Ainda não é Inscrito Cadastre-se</a>
         </form>
     </div>
+    <?php
+        if (isset($_POST['Email'])) {
+            $Email  = addslashes($_POST['Email']);
+            $Senha  = addslashes($_POST['Senha']);
+            if (!empty($Email) && !empty($Senha) ) {
+                $u->conectar("banco", "localhost", "root", "");
+                if ($u->msg == "") {
+                
+                if($u->logar($Email,$Senha)){
+                    header("location:painel.php");
+                }else{
+                    ?>
+                    <div class="msg-erro">
+                        Email ou senha incorretos!
+                    </div>
+            <?php
+                }
+            }else{
+                echo "Erro " . $u->msg;
+            }
+            
+        }else{
+            ?>
+            <div class="msg-erro">
+                Preencha todos os campos!
+            </div>
+    <?php
+        }
+    }
+    ?>
+
 </body>
--- painel.php
+++ painel.php
@@ -1,2 +1,7 @@
 <?php
+session_start();
+if(!isset($_SESSION['ID_USUARIO'])){
+    header("location:index,php");
+    exit;
+}
 echo "bem-vindo!";
